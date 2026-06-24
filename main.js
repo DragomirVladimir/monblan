08d0401b7c3da83e88ec6bc256b3395f49811656
DragomirVladimir <drag.v777@gmail.com>
2026-06-24 15:53:00 +0300

    swap layout switcher button states and update styles for active state

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import 'flatpickr/dist/flatpickr.min.css';
-import { initHeaderHeight } from './src/js/utils/getHeaderHeight';
-import { initViewToggle } from './src/js/viewToggle';
-import { initRenderPosts } from './src/js/modules/renderPosts';
-import { initDatepicker } from './src/js/modules/datepicker';
+import "flatpickr/dist/flatpickr.min.css";
+import { initHeaderHeight } from "./src/js/utils/getHeaderHeight";
+import { initViewToggle } from "./src/js/modules/viewToggle";
+import { initRenderPosts } from "./src/js/modules/renderPosts";
+import { initDatepicker } from "./src/js/modules/datepicker";
 
-initHeaderHeight('#header');
+initHeaderHeight("#header");
 initViewToggle();
 initRenderPosts();
 initDatepicker();
